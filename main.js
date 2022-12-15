f6e9bdd722624756c38deb23f4fb83ae81c6304d
mohamedkaram24682 <120126408+mohamedkaram24682@users.noreply.github.com>
2022-12-15 21:17:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var darkBtn = document.getElementById("dark-btn");
+darkBtn.onclick = function() {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+
+
+}
